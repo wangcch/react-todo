951fb48afb07323e7bc011c74c320951ba956614
wangcch <ci713@qq.com>
2018-06-05 18:04:18 +0800

    update(checkbox): select status and sort udpate

--- src/todo.js
+++ src/todo.js
@@ -7,8 +7,17 @@ class Todo extends Component {
     super(props);
 
     this.state = {
-      todoLists: ['demo01']
+      todoLists: [{
+        id: 0,
+        text: 'demo01',
+        done: true,
+        date: ''
+      }]
     }
+  }
+
+  _listSort = (a, b) => {
+    return b.id - a.id
   }
 
   isEnter = (code) => {
@@ -18,29 +27,42 @@ class Todo extends Component {
   inputEnter = (e) => {
     if (this.isEnter(e.keyCode) || this.isEnter(e.which)) {
       if (e.target.value) {
-        let list = this.state.todoLists
-        list.push(e.target.value)
-        this.setState({ list })
-        e.target.value = ''
+        let list = this.state.todoLists;
+        let item = {};
+        const date = new Date()
+        item.id = date.getTime();
+        item.text = e.target.value;
+        item.done = false;
+        item.date = date;
+        list.push(item)
+        list = list.sort(this._listSort);
+        this.setState({ list });
+        e.target.value = '';
       }
     }
   }
 
   delItem = (index) => {
     console.log('del: ' + index)
-    let list = this.state.todoLists
-    list.splice(index, 1)
-    this.setState({ list })
+    let list = this.state.todoLists;
+    list.splice(index, 1);
+    this.setState({ list });
+  }
+
+  toggleSelect = (index) => {
+    console.log('done: ' + index);
+    let list = this.state.todoLists;
+    list[index].done = !this.state.todoLists[index].done;
+    this.setState({ list });
   }
 
   render() {
     let TodoList = (props) => {
-      let todoList = props.list.concat();
-      todoList.reverse();
+      let todoList = props.list;
       return (
         <div>
           {todoList.map((item, index) =>
-            <TodoItem key={item.toString() + index} data={item} onDel={this.delItem.bind(this, index)}/>
+            <TodoItem key={item.text + index} data={item} onDel={this.delItem.bind(this, index)} onSelect={this.toggleSelect.bind(this, index)}/>
           )}
         </div>
       );
--- src/todoItem.js
+++ src/todoItem.js
@@ -6,8 +6,8 @@ class TodoItem extends Component {
     const {data} = this.props;
     return (
       <div className="todo-item todo-item_panel">
-        <input type="checkbox" className="check"/>
-        <p className="text">{data}</p>
+        <input type="checkbox" className="check" onChange={this.props.onSelect} checked={data.done}/>
+        <p className={data.done ? 'text done' : 'text'}>{data.text}</p>
         <span className="btn_del" onClick={this.props.onDel}>Del</span>
       </div>
     );
